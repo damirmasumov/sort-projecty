3189a32c93660a04bda17239ca34082c64b14607
holat <>
2025-12-11 00:44:18 +0300

    добавил функции

--- func.h
+++ func.h
@@ -8,3 +8,14 @@ typedef struct Node {
 } Node;
 
 int is_empty(Node* head);
+Node* create_node(int value);
+void push(Node** head, int value);
+int pop(Node** head);
+void print_stack(Node* head);
+void free_stack(Node* head);
+Node* generate_random_stack(int count);
+Node* copy_stack(Node* head);
+Node* insertion_sort_list(Node* head);
+void split_list(Node* head, Node** left, Node** right);
+Node* merge_lists(Node* a, Node* b);
+Node* merge_sort_list(Node* head);
